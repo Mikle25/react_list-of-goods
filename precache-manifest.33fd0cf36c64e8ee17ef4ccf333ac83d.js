17a841df88b2b4593c0287731af0798c225db0b5
Mikle25 <mikle2593@gmail.com>
2020-09-03 15:01:58 +0300

    Updates

---
+++ precache-manifest.33fd0cf36c64e8ee17ef4ccf333ac83d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa533f1901a8e9116db4dc0cb5e342f6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "25d9cfc4f2b69538f73f",
+    "url": "./static/css/main.770b457c.chunk.css"
+  },
+  {
+    "revision": "0deea3e5989f4fd07403",
+    "url": "./static/js/2.bc235ad9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bc235ad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d9cfc4f2b69538f73f",
+    "url": "./static/js/main.8e06e691.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
